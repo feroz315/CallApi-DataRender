b36abfc4345cb8bda404f77bb3985f8f4dff4887
FEROZ KHAN <Feroz3151@gmail.com>
2023-11-26 13:46:17 +0500

    CallAPI-Data

---
+++ App.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Productdata from "./CallApi/Apidata";
+
+function App() {
+  return (
+      <Productdata />
+  );
+}
+
+export default App;
---
+++ CallApi/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Header = () => {
+ return (
+    <div className='Header shopping-cart'>
+      <div className='Heading'>Shopping Cart</div>
+     <div>Cart
+     <sup>{'0'}</sup>
+       </div>
+    </div>
+  )
+}
+
+
+
+export default Header;
